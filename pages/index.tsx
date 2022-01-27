ff10cd1878e4b1a22f0014598c959897fafaa602
Sehal Sein <sehal.sein@fibonalabs.com>
2022-01-27 19:59:57 +0530

    Call Updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import fetcher from "../src/lib/swr";
 import logo from "../public/logo.png";
 import { IAPIResponseData } from "./api/users";
 import { IApiPost } from "./api/posts";
+import { Disclosure, Transition } from "@headlessui/react";
 
 const Home: NextPage<IAPIResponseData> = ({ menu, user, fallback }) => {
   const { data, error, loading } = usePostList({
@@ -13,7 +14,7 @@ const Home: NextPage<IAPIResponseData> = ({ menu, user, fallback }) => {
 
   return (
     <>
-      <header className="flex bg-primary text-white h-20 items-center">
+      <header className="flex bg-primary text-white h-14 md:h-20 items-center">
         <div className="max-w-7xl mx-auto flex gap-4 justify-between w-full px-4">
           <div className="h-10 w-1/6 relative">
             <NextImage
@@ -34,9 +35,9 @@ const Home: NextPage<IAPIResponseData> = ({ menu, user, fallback }) => {
         </div>
       </header>
 
-      <main className="max-w-7xl mx-auto py-16 px-4">
+      <main className="max-w-7xl mx-auto py-8 md:py-16 px-4">
         <div className="flex flex-1 flex-col gap-2 items-center">
-          <div className="bg-slate-300 h-32 w-32 rounded-full relative overflow-hidden">
+          <div className="bg-slate-300 h-24 w-24 md:h-32 md:w-32 rounded-full relative overflow-hidden">
             <NextImage
               alt="profile-pic"
               src={user.avatar}
@@ -45,8 +46,10 @@ const Home: NextPage<IAPIResponseData> = ({ menu, user, fallback }) => {
               objectPosition="center"
             />
           </div>
-          <span className="font-bold text-primary text-2xl">{user.name}</span>
-          <p>Designer, Vlogger, Teacher</p>
+          <div className="flex flex-col items-center">
+            <span className="font-bold text-primary text-2xl">{user.name}</span>
+            <p>Designer, Vlogger, Teacher</p>
+          </div>
           {/* <div className="flex gap-4">
             <span>
               <span className="font-bold ">{user.followers}</span>
@@ -118,7 +121,7 @@ const Home: NextPage<IAPIResponseData> = ({ menu, user, fallback }) => {
         <div className="mt-8 md:mt-16">
           {!data && !loading && error && <span>Error</span>}
           {loading && (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 animate-pulse">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-4 animate-pulse">
               <div className="h-64 bg-slate-300 rounded-md"></div>
               <div className="h-64 bg-slate-300 rounded-md"></div>
               <div className="h-64 bg-slate-300 rounded-md"></div>
@@ -126,15 +129,19 @@ const Home: NextPage<IAPIResponseData> = ({ menu, user, fallback }) => {
               <div className="h-64 bg-slate-300 rounded-md"></div>
             </div>
           )}
+
           {!loading && data && (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {data.map(({ title, id, image, currency, price }) => (
-                <div key={id} className="flex flex-col gap-2">
-                  <div className="h-80 bg-slate-300 rounded-md relative overflow-hidden cursor-pointer">
-                    <div className="absolute z-10 right-2 top-2 p-1 rounded-full backdrop-blur-sm bg-black/30">
+            <>
+              <Disclosure as="div" className="mt-2">
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className="flex items-center justify-between h-16 w-full px-4 py-2 text-sm font-medium text-left text-orange-900 bg-orange-100 rounded-lg hover:bg-orange-200 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-75">
+                      <span className="text-xl font-semibold">My Store</span>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6 text-white"
+                        className={`${
+                          open ? "transform rotate-180" : ""
+                        } w-6 h-6 text-orange-500`}
                         fill="none"
                         viewBox="0 0 24 24"
                         stroke="currentColor"
@@ -143,27 +150,151 @@ const Home: NextPage<IAPIResponseData> = ({ menu, user, fallback }) => {
                           strokeLinecap="round"
                           strokeLinejoin="round"
                           strokeWidth={2}
-                          d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                          d="M19 9l-7 7-7-7"
                         />
                       </svg>
-                    </div>
-                    <NextImage
-                      alt={title}
-                      src={image}
-                      className="object-cover object-center h-64 w-full overflow-hidden rounded-md"
-                      layout="fill"
-                    />
-                  </div>
-                  <div className="flex flex-col">
-                    <span className="text-lg font-bold ">{title}</span>
-                    <span className="text-gray-400 font-medium">
-                      {currency}
-                      {price}
-                    </span>
-                  </div>
-                </div>
-              ))}
-            </div>
+                    </Disclosure.Button>
+                    <Transition
+                      enter="transition duration-100 ease-out"
+                      enterFrom="transform scale-95 opacity-0"
+                      enterTo="transform scale-100 opacity-100"
+                      leave="transition duration-75 ease-out"
+                      leaveFrom="transform scale-100 opacity-100"
+                      leaveTo="transform scale-95 opacity-0"
+                    >
+                      <Disclosure.Panel className="pt-4 pb-2">
+                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                          {data.map(({ title, id, image, currency, price }) => (
+                            <div key={id} className="flex flex-col gap-2">
+                              <div className="h-56 bg-slate-300 rounded-md relative overflow-hidden cursor-pointer">
+                                <div className="absolute z-10 right-2 top-2 p-1 rounded-full backdrop-blur-sm bg-black/30">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6 text-white"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      strokeWidth={2}
+                                      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                                    />
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      strokeWidth={2}
+                                      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    />
+                                  </svg>
+                                </div>
+                                <NextImage
+                                  alt={title}
+                                  src={image}
+                                  className="object-cover object-center h-64 w-full overflow-hidden rounded-md"
+                                  layout="fill"
+                                />
+                              </div>
+                              <div className="flex flex-col">
+                                <span className="text-lg font-bold ">
+                                  {title}
+                                </span>
+                                <span className="text-gray-400 font-medium">
+                                  {currency}
+                                  {price}
+                                </span>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </Disclosure.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Disclosure>
+              <Disclosure as="div" className="mt-2">
+                {({ open }) => (
+                  <>
+                    <Disclosure.Button className="flex items-center justify-between h-16 w-full px-4 py-2 text-sm font-medium text-left text-orange-900 bg-orange-100 rounded-lg hover:bg-orange-200 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-75">
+                      <span className="text-xl font-semibold">Sound Cloud</span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className={`${
+                          open ? "transform rotate-180" : ""
+                        } w-6 h-6 text-orange-500`}
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M19 9l-7 7-7-7"
+                        />
+                      </svg>
+                    </Disclosure.Button>
+                    <Transition
+                      enter="transition duration-100 ease-out"
+                      enterFrom="transform scale-95 opacity-0"
+                      enterTo="transform scale-100 opacity-100"
+                      leave="transition duration-75 ease-out"
+                      leaveFrom="transform scale-100 opacity-100"
+                      leaveTo="transform scale-95 opacity-0"
+                    >
+                      <Disclosure.Panel className="pt-4 pb-2">
+                        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                          {data.map(({ title, id, image, currency, price }) => (
+                            <div key={id} className="flex flex-col gap-2">
+                              <div className="h-56 bg-slate-300 rounded-md relative overflow-hidden cursor-pointer">
+                                <div className="absolute z-10 right-2 top-2 p-1 rounded-full backdrop-blur-sm bg-black/30">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-6 w-6 text-white"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                  >
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      strokeWidth={2}
+                                      d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                                    />
+                                    <path
+                                      strokeLinecap="round"
+                                      strokeLinejoin="round"
+                                      strokeWidth={2}
+                                      d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    />
+                                  </svg>
+                                </div>
+                                <NextImage
+                                  alt={title}
+                                  src={image}
+                                  className="object-cover object-center h-64 w-full overflow-hidden rounded-md"
+                                  layout="fill"
+                                />
+                              </div>
+                              <div className="flex flex-col">
+                                <span className="text-lg font-bold ">
+                                  {title}
+                                </span>
+                                <span className="text-gray-400 font-medium">
+                                  {currency}
+                                  {price}
+                                </span>
+                              </div>
+                            </div>
+                          ))}
+                        </div>
+                      </Disclosure.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Disclosure>
+            </>
           )}
         </div>
       </main>
