315aab1ba809fae9a4627006e87dd6b663252273
Sehal Sein <sehal.sein@fibonalabs.com>
2022-02-21 20:14:28 +0530

    Footer Updated

--- src/components/layout/AppLayout/index.tsx
+++ src/components/layout/AppLayout/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import NextImage from "next/image";
-import logo from "@/public/logo.png";
+import logo from "@/public/logo-white.png";
 
 const AppLayout: React.FC = ({ children }) => {
   return (
-    <>
-      <header className="bg-primary flex h-14 items-center text-white md:h-20">
+    <div className="flex h-screen flex-col">
+      {/* <header className="bg-primary flex h-14 items-center text-white md:h-20">
         <div className="mx-auto flex w-full max-w-7xl justify-between gap-4 px-4">
           <div className="relative h-6 w-1/6 md:h-10">
             <NextImage
@@ -17,9 +17,22 @@ const AppLayout: React.FC = ({ children }) => {
             />
           </div>
         </div>
-      </header>
-      <main className="mx-auto max-w-7xl py-8 px-4 md:py-16">{children}</main>
-    </>
+      </header> */}
+      <main className="mx-auto max-w-7xl flex-grow py-8 px-4 md:py-16">
+        {children}
+      </main>
+      <footer className="w-full flex-initial py-10 text-center">
+        <div className="relative m-auto h-6 w-20">
+          <NextImage
+            src={logo}
+            alt="logo"
+            layout="fill"
+            objectFit="contain"
+            objectPosition="left"
+          />
+        </div>
+      </footer>
+    </div>
   );
 };
 
